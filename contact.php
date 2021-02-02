1a9a936f5f7aa8d58c3bb637c7ca15d51fee905d
NoSprinkles2000 <78176953+NoSprinkles2000@users.noreply.github.com>
2021-02-01 18:18:48 -0700

    Add files via upload

--- contact.php
+++ contact.php
@@ -3,7 +3,7 @@
 	<head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>The Golden</title>
+	<title>Shop &mdash; Free Website Template, Free HTML5 Template by gettemplates.co</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="description" content="Free HTML5 Website Template by gettemplates.co" />
 	<meta name="keywords" content="free website templates, free html5, free template, free bootstrap, free website template, html5, css3, mobile first, responsive" />
@@ -47,6 +47,7 @@
 	<!-- Flexslider  -->
 	<link rel="stylesheet" href="css/flexslider.css">
 
+	
 	<!-- Owl Carousel  -->
 	<link rel="stylesheet" href="css/owl.carousel.min.css">
 	<link rel="stylesheet" href="css/owl.theme.default.min.css">
@@ -67,60 +68,60 @@
 	<div class="fh5co-loader"></div>
 	
 	<div id="page">
-		<nav class="fh5co-nav" role="navigation">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-3 col-xs-2">
-						<div id="fh5co-logo"><a href="index.php">The Golden</a></div>
+		<div id="page">
+			<nav class="fh5co-nav" role="navigation">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-3 col-xs-2">
+							<div id="fh5co-logo"><a href="index.php">The Golden</a></div>
+						</div>
+						<div class="col-md-6 col-xs-6 text-center menu-1">
+							<ul>
+								<li><a href="./single.html">Tienda</a></li>
+								<li><a href="./about.php">Acerca de</a></li>
+								<li><a href="./contact.php">Contacto</a></li>
+								<li><a href="./contact.html">Ingresar</a></li>
+							</ul>
+						</div>
+						<div class="col-md-3 col-xs-4 text-right hidden-xs menu-2">
+							<ul>
+								<li class="search">
+									<div class="input-group">
+									  <input type="text" placeholder="Buscar..">
+									  <span class="input-group-btn">
+										<button class="btn btn-primary" type="button"><i class="icon-search"></i></button>
+									  </span>
+									</div>
+								</li>
+								<li class="shopping-cart"><a href="#" class="cart"><span><small>0</small><i class="icon-shopping-cart"></i></span></a></li>
+							</ul>
+						</div>
 					</div>
-					<div class="col-md-6 col-xs-6 text-center menu-1">
-						<ul>
-							<li><a href="./single.html">Tienda</a></li>
-							<li><a href="./about.php">Acerca de</a></li>
-							<li><a href="./contact.php">Contacto</a></li>
-							<li><a href="./contact.html">Ingresar</a></li>
-						</ul>
-					</div>
-					<div class="col-md-3 col-xs-4 text-right hidden-xs menu-2">
-						<ul>
-							<li class="search">
-								<div class="input-group">
-								  <input type="text" placeholder="Buscar..">
-								  <span class="input-group-btn">
-									<button class="btn btn-primary" type="button"><i class="icon-search"></i></button>
-								  </span>
-								</div>
-							</li>
-							<li class="shopping-cart"><a href="#" class="cart"><span><small>0</small><i class="icon-shopping-cart"></i></span></a></li>
-						</ul>
-					</div>
+					
 				</div>
-				
-			</div>
-		</nav>
-
-	<div id="fh5co-about">
+			</nav>
+	
+	<div id="fh5co-contact">
 		<div class="container">
-			<div class="about-content">
-				<div class="row animate-box">
-					<div class="col-md-6">
-						<div class="desc">
-							<h3>Nuestra historia</h3>
-							<p>En 2021 iniciamos como parte de un proyecto escolar de base de datos y ahora mismo estamos posicionados en una de las cadenas más grandes de distribución de vinos.</p> 
-							<p>A medida que fuimos creciendo nos dimos cuenta de la importancia del medio ambiente y del cuidado de la fauna silvestre.</p>
-						</div>
-						<div class="desc">
-							<h3>Misión &amp; Visión</h3>
-							<p>Nuestra misión es salvar a los leopardos del amazonas con base al cuidado de su ecosistema.</p> 
-							<p>Nuestra visión es estar posicionado como el mayor distribuidor de América Latina.</p>
-						</div>
+			<div class="row">
+				<div class="col-md-6 col-md-push-5 animate-box">
+					
+					<div class="fh5co-contact-info">
+						<h3>Información de contacto</h3>
+						<ul>
+							<li class="address">Calle The Game, Mazatlán, Sinaloa.</li>
+							<li class="phone"><a href="tel://6691892060">+52 669 189 2060</a></li>
+							<li class="email"><a href="mailto:thegolden@gmail.com">thegolden@gmail.com</a></li>
+							<li class="url"><a href="http://thegolden.com">thegolden.com</a></li>
+						</ul>
 					</div>
-					<div class="col-md-6">
-						<img class="img-responsive" src="images/logos/TheGolden.png" alt="about">
-					</div>
+
 				</div>
 			</div>
 			
+		</div>
+	</div>
+
 
 	<footer id="fh5co-footer" role="contentinfo">
 		<div class="container">
@@ -180,7 +181,10 @@
 	<script src="js/jquery.countTo.js"></script>
 	<!-- Flexslider -->
 	<script src="js/jquery.flexslider-min.js"></script>
-	
+	<!-- Google Map -->
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCefOgb1ZWqYtj7raVSmN4PL2WkTrc-KyA&sensor=false"></script>
+	<script src="js/google_map.js"></script>
+
 	<!-- Main -->
 	<script src="js/main.js"></script>
 
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	<head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Shop &mdash; Free Website Template, Free HTML5 Template by gettemplates.co</title>
+	<title>The Golden</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="description" content="Free HTML5 Website Template by gettemplates.co" />
 	<meta name="keywords" content="free website templates, free html5, free template, free bootstrap, free website template, html5, css3, mobile first, responsive" />
@@ -77,7 +77,7 @@
 					<ul>
 						<li><a href="./single.html">Tienda</a></li>
 						<li><a href="./about.php">Acerca de</a></li>
-						<li><a href="./contact.html">Contacto</a></li>
+						<li><a href="./contact.php">Contacto</a></li>
 						<li><a href="./contact.html">Ingresar</a></li>
 					</ul>
 				</div>
